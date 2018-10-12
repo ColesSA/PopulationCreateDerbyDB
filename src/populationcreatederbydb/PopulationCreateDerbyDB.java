531ae5fc1274093683e1ec7c11345a0bd5847a35
Stephen Coles <39338313+ColesSA@users.noreply.github.com>
2018-10-12 10:20:58 -0400

    Final

--- src/populationcreatederbydb/PopulationCreateDerbyDB.java
+++ src/populationcreatederbydb/PopulationCreateDerbyDB.java
@@ -5,24 +5,27 @@ import java.sql.*;
 public class PopulationCreateDerbyDB {
 
     public static void main(String[] args) {
+        // Java Database Connectivity in contrast to ODBC.
+        // Database program, but not a management system. (RDBMS)
         String dbProtocol = "jdbc:derby:";
-        String dbDirectory = "e:/Bus Prgrm concpt/Projects/PopulationCreateDerbyDB/";
-        String dbName = "PopulationDB2";
-        String dbExtra = ";create=true";
+        String dbDirectory = "C:/Users/55colessa31/Documents/GitHub/PopulationCreateDerbyDB/";
+        String dbName = "PopulationDB"; // Database Name.
+        String dbExtra = ";create=true"; // Create the database schema.
         String userName = "Admin";
         String password = "MuCis";
         String connectionCreateURL = dbProtocol + dbDirectory + dbName + dbExtra;
         String shutDownURL = dbProtocol + ";shutdown = true";
-        String createStatement = "CREATE TABLE Population "
+        String createTable = "CREATE TABLE Population "
                 + "(TownNumber INTEGER PRIMARY KEY, "
                 + "TownName VARCHAR(20), "
                 + "CountyNumber INTEGER, "
                 + "RegionNumber INTEGER, "
                 + "Population INTEGER)";
+        
         try {
             Connection connection1 = DriverManager.getConnection(connectionCreateURL, userName, password);
             Statement statement1 = connection1.createStatement();
-            statement1.execute(createStatement);
+            statement1.execute(createTable);
             DriverManager.getConnection(shutDownURL);
         } catch (SQLException e) {
             System.out.println(e.toString());
